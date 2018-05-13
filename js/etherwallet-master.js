b663f78d7e73489398a1f5dc329a001aa89b9c4d
Admin <Admin@DESKTOP-01MJO1I.(none)>
2018-05-13 08:19:24 +0700

    Update

--- js/etherwallet-master.js
+++ js/etherwallet-master.js
@@ -5954,7 +5954,7 @@ nodes.nodeList = {
         'blockExplorerAddr': 'https://explorer.etherfact.org/addr/[[address]]',
         'type': nodes.nodeTypes.ETHF,
         'eip155': true,
-        'chainId': '',
+        'chainId': '1',
         'tokenList': [],
         'abiList': [],
         'estimateGas': true,
